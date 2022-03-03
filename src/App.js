96770eb84c90e1476e64e07cd4605cb6bd349c2d
baolongt <tranbaolong14@gmail.com>
2022-03-03 15:09:24 +0700

    change layout and sider

--- src/App.js
+++ src/App.js
@@ -4,40 +4,42 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ClubList from "../src/pages/club/club-demo/club-list.container";
 import "./App.less";
 import DashboardLayout from "./layouts/dashboard/dashboard.layout";
+import DashboardSider from "./layouts/dashboard/dashboard.sider";
+import ClubSider from "./layouts/club/club.sider";
 import HomeLayout from "./layouts/home";
 import Welcome from "./pages/dashboard/welcome";
 import Login from "./pages/home/login";
 import { store } from "./redux/store";
 
 const AppWrapper = () => {
-  return (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<App />
+		</Provider>
+	);
 };
 const App = () => {
-  const { user } = useSelector((state) => state.user);
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="" element={<HomeLayout />}>
-          <Route path="" element={<Login />} />
-          <Route path="/login" element={<Login />} />
-        </Route>
-        {user !== null ? (
-          <Route path="dashboard" element={<DashboardLayout />}>
-            <Route path="" element={<Welcome />} />
-            <Route path="clubs" element={<ClubList />} />
-          </Route>
-        ) : (
-          <Route path="dashboard" element={<DashboardLayout />}>
-            <Route path="" element={<Welcome />} />
-          </Route>
-        )}
-      </Routes>
-    </BrowserRouter>
-  );
+	const { user } = useSelector((state) => state.user);
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="" element={<HomeLayout />}>
+					<Route path="" element={<Login />} />
+					<Route path="/login" element={<Login />} />
+				</Route>
+				<Route
+					path="dashboard"
+					element={<DashboardLayout sider={<DashboardSider />} />}
+				>
+					<Route path="" element={<Welcome />} />
+					<Route path="clubs" element={<ClubList />} />
+				</Route>
+				<Route path="club" element={<DashboardLayout sider={<ClubSider />} />}>
+					<Route path="" element={<Welcome />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default AppWrapper;
